ec0d7bd460d8be2881435d6698d485c8f8e81cf0
Abraao <ese_aoliveira@uolinc.com>
2019-04-07 16:50:01 -0300

    add Watson natrural language undestanding service

--- robots/text.js
+++ robots/text.js
@@ -2,66 +2,102 @@ const algorithmia = require('algorithmia')
 const algorithmiaApiKey = require('../credentials/algorithm.json').apiKey
 const sentenceBoundaryDetection = require('sbd')
 
-async function robot(content) {
-  
-    fetchContentFromWikipedia = async (content) => {
-        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey) 
-        const wikipediaAlgorithm =  algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
-        const wikipediaResponde = await wikipediaAlgorithm.pipe(content.searchTerm)
-        const wikipediaContent = wikipediaResponde.get()
-        content.sourceContentOriginal = wikipediaContent.content;
-    }
-
-    sanitizeContent = async (content) => {
-        removeBlankLinesAndMarkdown = (text) => {
-            const allLines = text.split('\n');
-
-            const removedMarkDownAndBlankLines =  allLines.filter((line) => {  
-                if(line.trim().length === 0 || line.trim().startsWith('=')) {
-                    return false
-                } 
-                return true
-            })
-
-            return removedMarkDownAndBlankLines.join(' ')
-        }
-
-        removeDateInParentheses = (text) => {
-            return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ');
-            
-        }
-
-        breakContentIntoSentences = (content) => {
-            content.sentences = []    
-            const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized) 
-            sentences.forEach((sentence) => {
-                content.sentences.push({
-                    text: sentence,
-                    keywords: [],
-                    images: []
-                })
-            })
-
-            console.log(content.sentences);
-           
-
-        }
-
-          const withoutBlankLinesAndMarkDown = removeBlankLinesAndMarkdown( content.sourceContentOriginal );
-          const withoutDateInParentheses = removeDateInParentheses(withoutBlankLinesAndMarkDown);
-          content.sourceContentSanitized = withoutDateInParentheses;  
-          
-         
-    }
-
-
-    await fetchContentFromWikipedia(content);
-    await sanitizeContent(content)
-    await breakContentIntoSentences(content)
+const watsonApiKey = require('../credentials/watson-nlu.json').apikey
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js')
  
-  
+const nlu = new NaturalLanguageUnderstandingV1({
+  iam_apikey: watsonApiKey,
+  version: '2018-04-05',
+  url: 'https://gateway.watsonplatform.net/natural-language-understanding/api/'
+})
+
+
+async function robot(content) {
+
+  await fetchContentFromWikipedia(content)
+  sanitizeContent(content)
+  breakContentIntoSentences(content)
+  limitMaximumSentences(content)
+  await fetchKeywordsOfAllSentences(content)
+
+  async function fetchContentFromWikipedia(content) {
+    const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+    const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+    const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+    const wikipediaContent = wikipediaResponse.get()
+
+    content.sourceContentOriginal = wikipediaContent.content
+  }
+
+  function sanitizeContent(content) {
+    const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+    const withoutDatesInParentheses = removeDatesInParentheses(withoutBlankLinesAndMarkdown)
+
+    content.sourceContentSanitized = withoutDatesInParentheses
+
+    function removeBlankLinesAndMarkdown(text) {
+      const allLines = text.split('\n')
+
+      const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+        if (line.trim().length === 0 || line.trim().startsWith('=')) {
+          return false
+        }
+
+        return true
+      })
+
+      return withoutBlankLinesAndMarkdown.join(' ')
+    }
+  }
+
+  function removeDatesInParentheses(text) {
+    return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+  }
+
+  function breakContentIntoSentences(content) {
+    content.sentences = []
+
+    const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+    sentences.forEach((sentence) => {
+      content.sentences.push({
+        text: sentence,
+        keywords: [],
+        images: []
+      })
+    })
+  }
+
+  function limitMaximumSentences(content) {
+    content.sentences = content.sentences.slice(0, content.maximumSentences)
+  }
+
+  async function fetchKeywordsOfAllSentences(content) {
+    for (const sentence of content.sentences) {
+      sentence.keywords = await fetchWatsonAndReturnKeywords(sentence.text)
+    }
+  }
+
+  async function fetchWatsonAndReturnKeywords(sentence) {
+    return new Promise((resolve, reject) => {
+      nlu.analyze({
+        text: sentence,
+        features: {
+          keywords: {}
+        }
+      }, (error, response) => {
+        if (error) {
+          throw error
+        }
+
+        const keywords = response.keywords.map((keyword) => {
+          return keyword.text
+        })
+
+        resolve(keywords)
+      })
+    })
+  }
+
 }
-
-
 
 module.exports = robot
